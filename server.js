662871a83b6255accd6d9124090eb741572f9ddc
jeff tansey <jeff.tansey@gmail.com>
2021-08-17 17:13:52 -0400

    init

---
+++ server.js
@@ -0,0 +1,44 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 8080; // set our port
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+router.get('/true', function(req, res) {
+	res.json({ message: 'true' });	
+});
+
+router.get('/false', function(req, res) {
+	res.json({ message: 'false' });	
+});
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
